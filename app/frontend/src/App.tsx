61f149aebdde12dc4073b8ec587e42e760a77e95
Sakari Palojoki <palojoki.sakari@gmail.com>
2023-02-22 20:19:35 +0200

    Init base layout

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const App = () => {
   return(
-    <div>Moi</div>
+    <div className='flex flex-col lg:grid lg:grid-cols-[5fr_2fr] lg:grid-rows-[1fr_auto_5fr] h-screen bg-violet-500'>
+      <div className='bg-green-500 aspect-video'>Video</div> {/*Video*/}
+      <div className='bg-violet-500 flex-shrink'>Toolbar</div> {/*Reactions*/}
+      <div className='bg-blue-500 flex-grow lg:col-start-2 lg:col-end-3 lg:row-span-full'>Chat</div> {/*Chat*/}
+    </div>
   )
 }
 
