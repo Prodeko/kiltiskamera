1f3ff972c2f2f5998ae7408e3ceeed351a233c3a
Leevi Jäkälä <jakala.leevi@gmail.com>
2023-02-22 19:34:13 +0200

    Add ws server

--- app/backend/index.ts
+++ app/backend/index.ts
@@ -1,12 +1,31 @@
 import express from 'express';
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({
+  port: 8080,
+});
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Worlds!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen()
+
+const start = () => {
+    wss.on('connection', (socket) => {
+        console.log("CONNECTED");
+
+        socket.on("message", (data) => {
+            console.log('received: %s', data);
+        })
+    });
+
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+}
+
+start();
