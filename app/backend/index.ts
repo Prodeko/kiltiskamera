4ab3fada0161c3aeb3076dfc34c5d3f8d32bb0cc
Leevi Jäkälä <jakala.leevi@gmail.com>
2023-02-22 21:15:08 +0200

    parse ChatMessages

--- app/backend/index.ts
+++ app/backend/index.ts
@@ -1,15 +1,34 @@
-import { WebSocketServer } from 'ws';
+import { RawData, WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({
   port: 8080,
 });
+
+type ChatMessage = {
+  timestamp: Date,
+  text: string
+};
+
+const handleMessage = (data: RawData) => {
+  const { timestamp, text } = JSON.parse(data.toString());
+  if (!timestamp || !text) {
+    throw new Error('Invalid message');
+  }
+
+  const msg = { timestamp, text } as ChatMessage;
+  console.log('Received valid message', msg);
+};
 
 const start = () => {
   wss.on('connection', (socket) => {
     console.log('CONNECTED');
 
     socket.on('message', (data) => {
-      console.log('received: %s', data);
+      try {
+        handleMessage(data);
+      } catch (err) {
+        console.log('Invalid message:', err);
+      }
     });
   });
 };
