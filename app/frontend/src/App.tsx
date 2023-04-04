32bc05b266c7554f57a152869f6d51c8890f408f
Sakari Palojoki <palojoki.sakari@gmail.com>
2023-04-04 21:05:26 +0300

    Add logic for displaying loading spinner

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -16,8 +16,8 @@ const App = () => {
   return(
     <div className='flex flex-col lg:grid lg:grid-cols-[5fr_2fr] lg:grid-rows-[1fr_auto_5fr] h-screen bg-violet-500'>
       <div className='aspect-video'>
-         <video className='bg-black aspect-video' autoPlay muted id={VIDEO_EL_ID}></video> 
-          <VideoLoading />
+         <video className={`bg-black aspect-video ${videoStatus ? '' : 'h-0 w-0'}`} autoPlay muted id={VIDEO_EL_ID} onPlay={() => setPlaying(true)} onPause={() => console.log('Teremos')}></video> 
+          {!videoStatus && <VideoLoading />}
       </div>
       <div className='lg:row-start-3 bg-blue-200 flex-shrink lg:h-full lg:flex-shrink-0'>Toolbar</div> {/*Reactions*/}
       <div className='flex-grow lg:col-start-2 lg:col-end-3 lg:row-span-full'>
