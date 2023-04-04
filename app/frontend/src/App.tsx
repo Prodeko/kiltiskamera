3522a9e2748ce80c8afabfb769b5821c6e9f938e
Leevi Jäkälä <jakala.leevi@gmail.com>
2023-04-04 19:56:51 +0300

    somewhat simplify janus interface

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -2,19 +2,17 @@ import React, { useEffect } from 'react'
 import Chat from './Chat'
 import { janusState } from './janus'
 
+const VIDEO_EL_ID = 'videoelement'
+
 const App = () => {
-  
-  const janusStateChange = (state: any) => {
-    //console.log('janusStateChange', state)
-  }
 
   useEffect(() => {
-    janusState.subscribe(janusStateChange)
+    janusState.startWithID(VIDEO_EL_ID)
   }, [])
 
   return(
     <div className='flex flex-col lg:grid lg:grid-cols-[5fr_2fr] lg:grid-rows-[1fr_auto_5fr] h-screen bg-violet-500'>
-      <video className='bg-black aspect-video' autoPlay muted id='remotevideo'></video>
+      <video className='bg-black aspect-video' autoPlay muted id={VIDEO_EL_ID}></video>
       <div className='lg:row-start-3 bg-blue-200 flex-shrink lg:h-full lg:flex-shrink-0'>Toolbar</div> {/*Reactions*/}
       <div className='flex-grow lg:col-start-2 lg:col-end-3 lg:row-span-full'>
         <Chat />
--- app/frontend/src/janus.js
+++ app/frontend/src/janus.js
@@ -1,5 +1,7 @@
 import { Janus } from 'janus-gateway'
 
+const SERVER_ADDR = "http://10.100.46.237:8088/janus"
+
 Janus.init({
   debug: true,
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -10,185 +12,118 @@ Janus.init({
 class JanusState {
 
   constructor() {
-    this.subscribers = []
+    this.statusSubs = []
     this.streaming = null
     this.selectedStream = null
     this.bitrateTimer = null
     this.status = null
-  }
-
-  /*
-  Add a callback to be notified when janusState changes
-  */
-  subscribe(cb) {
-    console.log('subscribed')
-    this.subscribers.push(cb)
-  }
-
-  notifySubscribers() {
-    this.subscribers.forEach(f => f(this.state))
+    this.videoID = null
   }
 
   get state() {
     return {
       streaming: this.streaming,
       selectedStream: this.selectedStream,
-      bitrateTimer: this.bitrateTimer,
-      status: this.status
+      status: this.status,
+      videoID: this.videoID
     }
   }
 
   setStatus(val) {
     this.status = val
-    this.notifySubscribers()
   }
 
   setStreaming(val) {
     this.streaming = val
-    this.notifySubscribers()
   }
 
   setSelectedStream(val) {
     this.selectedStream = val
-    this.notifySubscribers()
   }
-  
-  setBitrateTimer(val) {
-    this.bitrateTimer = val
-    this.notifySubscribers()
+
+  startWithID(id) {
+    this.videoID = id
   }
 }
 
 const janusState = new JanusState()
 
 const janus = new Janus({
-  server: "http://10.100.46.237:8088/janus",
+  server: SERVER_ADDR,
   success: function() {
-      // Attach to streaming plugin
       janus.attach(
         {
           plugin: "janus.plugin.streaming",
           opaqueId: "streamingtest-" + Janus.randomString(12),
           success: function(pluginHandle) {
             janusState.setStreaming(pluginHandle)
-            console.log("Plugin attached! (" + pluginHandle.getPlugin() + ", id=" + pluginHandle.getId() + ")");
-            //get available streams and select the one we need
             updateStreamsList();
           },
           error: function(error) {
             console.log("Error attaching plugin... " + error);
           },
-          onmessage: function(msg, jsep) {
-            Janus.debug(" ::: Got a message :::");
-            Janus.debug(msg);
-            var result = msg["result"];
-            if(result !== null && result !== undefined) {
-              if(result["status"] !== undefined && result["status"] !== null) {
-                var status = result["status"];
-                if(status === 'starting')
-                  Janus.log("Starting, please wait...");
-                else if(status === 'started')
-                  Janus.log("Started");
-                else if(status === 'stopped')
-                  Janus.log("Stopped");
-              }
-            } else if(msg["error"] !== undefined && msg["error"] !== null) {
-              alert(msg["error"]);
-              return;
-            }
-            if(jsep !== undefined && jsep !== null) {
-              Janus.debug("Handling SDP as well...");
-              Janus.debug(jsep);
-              // Offer from the plugin, let's answer
-              janusState.streaming.createAnswer(
-                {
-                  jsep: jsep,
-                  media: { audioSend: false, videoSend: false },	// We want recvonly audio/video
-                  success: function(jsep) {
-                    Janus.debug("Got SDP!");
-                    Janus.debug(jsep);
-                    var body = { "request": "start" };
-                    janusState.streaming.send({"message": body, "jsep": jsep});
-                  },
-                  error: function(error) {
-                    Janus.error("WebRTC error:", error);
-                    alert("WebRTC error... " + JSON.stringify(error));
-                  }
-                });
-            }
-          },
-          onremotetrack: function(track, mid, on) {
-            Janus.log("::: YEET A REMOTE STREAM :::")
-            Janus.log("track", track)
-            Janus.log("mid", mid)
-            Janus.log("on", on)
-            console.log(`Received remote track mid=${mid} on=${on}`, track);
-            const videoStream = new MediaStream([track]);
-            console.log(videoStream)
-            const el = document.getElementById('remotevideo')
-            console.log("ELEM", el)
-            Janus.attachMediaStream(el, videoStream)
-            // el.srcObject = videoStream
-            console.log("ELEM2", el)
-            // Janus.attachMediaStream($('#remotevideo').get(0), videoStream);
-          },
-          // onremotetrack: function(stream) {
-          //   console.log(" ::: Got a remote stream :::");
-          //   console.log(stream);
-          //   // Janus.attachMediaStream($('#remotevideo').get(0), stream);
-          //   console.log("ATTACH STREAM HERE", stream)
-          //   var videoTracks = stream.getVideoTracks();
-          //   if(videoTracks && videoTracks.length &&
-          //       (Janus.webRTCAdapter.browserDetails.browser === "chrome" ||
-          //         Janus.webRTCAdapter.browserDetails.browser === "firefox" ||
-          //         Janus.webRTCAdapter.browserDetails.browser === "safari")) {
-          //           janusState.setBitrateTimer(setInterval(function() {
-          //             Janus.debug("Current bitrate is " + janusState.streaming.getBitrate());
-          //         }, 1000))
-          //   }
-          // },
+          onmessage: handleMessage,
+          onremotetrack: handleTrack,
           oncleanup: function() {
-            Janus.log(" ::: Got a cleanup notification :::");
-            alert("streaming cleanup");
-            if(janusState.bitrateTimer !== null && janusState.bitrateTimer !== undefined)
-              clearInterval(janusState.bitrateTimer);
-            janusState.setBitrateTimer(null);
-            setInterval(function() {
-              Janus.debug("Current bitrate is " + janusState.streaming.getBitrate());
-            }, 1000);
+            console.log("cleanup")
           }
         });
   },
   error: function() {
-    console.log("JANUJS ERRRORR")
-    janusState.setStatus('error')
+    console.log('error')
   }
 })
 
-function startStream() {
-  Janus.log("Selected video id #" + janusState.selectedStream);
-	var body = { "request": "watch", id: parseInt(janusState.selectedStream) };
-	janusState.streaming.send({"message": body});
-	// No remote video yet
-	Janus.log("starting stream");
-}
-
 function updateStreamsList() {
 	const body = { "request": "list" };
-	Janus.debug("Sending message (" + JSON.stringify(body) + ")");
 	janusState.streaming.send({"message": body, success: function(result) {
-		if(result === null || result === undefined) {
-      Janus.debug("Got no response to our query for available streams");
-			return;
-		}
-		if(result["list"] !== undefined && result["list"] !== null) {
-			var list = result["list"];
-			Janus.log("Got a list of available streams");
-			Janus.log(list);
-      janusState.setSelectedStream(list[1]["id"])
+		if(result && result["list"]) {
+      const streamList = result["list"]
+      const stream = streamList[1] // 1 idx is the h264 stream
+      const streamId = stream["id"]
+      janusState.setSelectedStream(streamId)
       startStream();
 		}
 	}});
+}
+
+function startStream() {
+	var body = { "request": "watch", id: parseInt(janusState.selectedStream) };
+	janusState.streaming.send({"message": body});
+}
+
+const handleMessage = (msg, jsep) => {
+  var result = msg["result"];
+  if(result && result["status"]) {
+    console.log(result["status"])
+  } else if(msg["error"] !== undefined && msg["error"] !== null) {
+    console.log("error");
+    return;
+  }
+  if(jsep) {
+    janusState.streaming.createAnswer(
+      {
+        jsep: jsep,
+        media: { audioSend: false, videoSend: false },	// We want recvonly audio/video
+        success: function(jsep) {
+          var body = { "request": "start" };
+          janusState.streaming.send({"message": body, "jsep": jsep});
+        },
+        error: function(error) {
+          console.log("error");
+        }
+      });
+  }
+}
+
+const handleTrack = (track, mid, on) => {
+const videoStream = new MediaStream([track]);
+if (!janusState.videoID) {
+  console.log("ERROR: videoID missing. Forgot to call startWithID?")
+  return
+}
+const el = document.getElementById(janusState.videoID)
+Janus.attachMediaStream(el, videoStream)
 }
 
 export { Janus, janusState }
