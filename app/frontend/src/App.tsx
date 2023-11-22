a8121bb18e66617a24bdfc780c544ce7c8f806b1
aksun1 <aksunpostit@gmail.com>
2023-11-22 21:58:19 +0200

    Initial auth server and iframe embed

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,23 +1,12 @@
-import React, { useState, useEffect } from 'react'
 import Chat from './Chat'
-import VideoLoading from './VideoLoading'
-import { janusState } from './janus'
 
-const VIDEO_EL_ID = 'videoelement'
 
 const App = () => {
-  const [videoStatus, setVideoStatus] = useState<boolean>(false)
-
-  useEffect(() => {
-    janusState.startWithID(VIDEO_EL_ID)
-    janusState.subsribeToStatus((s: boolean) => setVideoStatus(s))
-  }, [])
 
   return(
     <div className='flex flex-col lg:grid lg:grid-cols-[5fr_2fr] lg:grid-rows-[1fr_auto_5fr] h-screen bg-violet-500'>
       <div className='aspect-video'>
-         <video className={`bg-black aspect-video ${videoStatus ? '' : 'h-0 w-0'}`} muted id={VIDEO_EL_ID} onPlay={() => setVideoStatus(true)} onPause={() => console.log('Teremos')}></video> 
-          {!videoStatus && <VideoLoading />}
+        <iframe src="http://prodekopi.local:8889/cam" title="livestream embed" className="h-full w-full"></iframe>
       </div>
       <div className='lg:row-start-3 bg-blue-200 flex-shrink lg:h-full lg:flex-shrink-0'>Toolbar</div> {/*Reactions*/}
       <div className='flex-grow lg:col-start-2 lg:col-end-3 lg:row-span-full'>
