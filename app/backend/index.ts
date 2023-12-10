1044e16c059779cefa8069994f606083bb0aefa4
aksun1 <aksunpostit@gmail.com>
2023-12-10 14:56:58 +0200

    Implement oauth2 integration and viewers list API

--- app/backend/index.ts
+++ app/backend/index.ts
@@ -1,13 +1,25 @@
 import { RawData, WebSocketServer, WebSocket } from 'ws';
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import crypto from 'crypto';
+import passport from 'passport'
+import OAuth2Strategy from 'passport-oauth2';
+import path from 'path';
+import session from 'express-session'
+import cookieParser from 'cookie-parser';
+import request, { get } from 'request'
 
 const userId = "dooku"; // need to be changed after auth integration
 const tokenValidityPeriod = 5 * 60 * 1000; // 5 minutes in milliseconds
 
 interface TokenInfo {
-  userId: string;
+  id: string;
+  displayName: string;
   timestamp: number;
+}
+interface ProdekoUser {
+  id: string
+  email: string
+  displayName: string
 }
 
 const onlineTokens: Record<string, TokenInfo> = {};
@@ -22,14 +34,18 @@ function generateRandomToken(): string {
   return randomString;
 }
 
-function addOnlineToken(userId: string): string {
+function addOnlineToken(user: ProdekoUser): string {
   const token = generateRandomToken();
-  onlineTokens[token] = { userId, timestamp: Date.now() };
+  onlineTokens[token] = { id: user.id, displayName: user.displayName, timestamp: Date.now() };
   return token;
 }
 
 function isTokenOnline(token: string): boolean {
   return !!onlineTokens[token];
+}
+
+function getOnlineTokens() {
+  return onlineTokens;
 }
 
 function removeExpiredTokens(): void {
@@ -77,16 +93,119 @@ app.get('/authenticate', (req: Request, res: Response) => {
   }
 });
 
-app.get('/stream_url', (req: Request, res: Response) => {
+app.use(cookieParser());
+app.use(session({ secret: 'YOUR_SESSION_SECRET', resave: false, saveUninitialized: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user: ProdekoUser, done) {
+  done(null, user);
+});
+passport.use(new OAuth2Strategy({
+  authorizationURL: 'http://localhost:8000/oauth2/auth',
+  tokenURL: 'http://localhost:8000/oauth2/token',
+  clientID: 'BlnkyR4JOOBTvtWGZYnF5KxtILfa2sbGL8fcqAD4',
+  clientSecret: 'SZsrN847Rg36OLlJ3GPQEvEplf3qPibtYIATcz3xTwyL6pZZr67QlMGNafE5Xw7Dvs7ATjFONBFazMtOJr5WxOcdLA5iX5DMhm59i1xdu3ywOKWutjG6GrdBpv4IeYOa',
+  callbackURL: 'http://localhost:8087/auth/google/callback',
+}, (accessToken: string, refreshToken: string, profile: string, done: Function) => {
+    // handle the user profile
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    }
+
+    // Get user details
+    const userResponse = request(
+      `http://localhost:8000/oauth2/user_details/`,
+      {
+        method: "GET",
+        headers,
+        json:true,
+                //https: {
+        //  rejectUnauthorized: !isDevOrTest,
+        //},
+      },
+    function(error: any, response: any, body: any) {
+      const { pk, email, first_name, last_name, has_accepted_policies } = response.body
+
+      //if (!has_accepted_policies) {
+      //  const e = new Error(
+      //    `You have not accepted Prodeko's privacy policy.
+    //Please accept our privacy policy in order to use the site while logged in.
+    //You may accept the policy by logging in via https://prodeko.org/login,
+    //and clicking 'I agree' on the displayed prompt.`.replace(/\n/g, " ")
+       // )
+        //e["code"] = "PRPOL"
+       // throw e
+     // }
+    done(null, {
+      id: pk,
+      displayName: `${first_name} ${last_name}`,
+      email: email,
+    });
+    })
+
+}));
+
+app.get('/auth/google/callback', passport.authenticate('oauth2', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+}));
+
+app.get('/login', (req, res) => {
+  res.send(`
+    <html>
+      <body>
+        <a href="/auth/google">Sign in with Google</a>
+      </body>
+    </html>
+  `);
+});
+
+// Middleware to check if the user is authenticated
+const ensureAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  // If not authenticated, redirect to Google authentication
+  res.redirect('/auth/google');
+};
+app.get('/auth/google', passport.authenticate('oauth2'));
+
+// Serve static files from the 'frontend/build' directory
+//app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// For any other routes, serve the React app
+//app.get('*', (req, res) => {
+//  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+//});
+
+app.use(ensureAuthenticated, express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('/stream_url', ensureAuthenticated, (req: Request, res: Response) => {
   // This endpoint will be called from frontend
   res.set('Access-Control-Allow-Origin', '*'); // TODO cleanup
-
-  var token = addOnlineToken(userId);
+  const user = req.user as ProdekoUser
+  console.log("stream user", req.user);
+  var token = addOnlineToken(user); 
   
   console.log("got stream url name");
   const m3u8_stream_source = "https://kiltiskamera.prodeko.org/live/hls/"+token+"/stream.m3u8"
 
   res.json({'url': m3u8_stream_source});
+});
+
+app.get('/viewers', ensureAuthenticated, (req: Request, res: Response) => {
+  // This endpoint returns the current viewers of the stream
+  
+  console.log("got viewer req");
+  
+  const tokens = getOnlineTokens()
+  res.json({'viewers': Object.keys(tokens).map(token => tokens[token].displayName)});
 });
 
 
