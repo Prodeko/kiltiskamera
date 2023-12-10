966b94b19219ebc4879a2d2c4d4481b9bd65138f
Leevi Jäkälä <jakala.leevi@gmail.com>
2023-12-10 15:00:33 +0200

    Lint the frontend components

--- app/frontend/src/VideoLoading.tsx
+++ app/frontend/src/VideoLoading.tsx
@@ -1,26 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import LoadingSpinner from './LoadingSpinner'
+import React, { useEffect, useState } from "react";
+import LoadingSpinner from "./LoadingSpinner";
 
-type BgColor = '#1D4ED8' | '#B91C1C' | '#C2410C' | '#ecd000' |'#15803D'
+type BgColor = "#1D4ED8" | "#B91C1C" | "#C2410C" | "#ecd000" | "#15803D";
 
-const bgColors: BgColor[] = ['#1D4ED8', '#B91C1C', '#C2410C', '#ecd000','#15803D']
+const bgColors: BgColor[] = [
+  "#1D4ED8",
+  "#B91C1C",
+  "#C2410C",
+  "#ecd000",
+  "#15803D",
+];
 
 const VideoLoading = () => {
-  const [bgColor, setBgColor] = useState<BgColor>(bgColors[0])
+  const [bgColor, setBgColor] = useState<BgColor>(bgColors[0]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setBgColor(bgColors[(bgColors.indexOf(bgColor) + 1) % bgColors.length])
-    }, 1000)
-    return () => clearInterval(interval)
-  }, [bgColor])
+      setBgColor(bgColors[(bgColors.indexOf(bgColor) + 1) % bgColors.length]);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [bgColor]);
 
-
-  return(
-    <div className={`w-full h-full flex items-center justify-center transition-all duration-700`} style={{backgroundColor: bgColor}}>
+  return (
+    <div
+      className={`w-full h-full flex items-center justify-center transition-all duration-700`}
+      style={{ backgroundColor: bgColor }}
+    >
       <LoadingSpinner />
     </div>
-  )
-}
+  );
+};
 
-export default VideoLoading
+export default VideoLoading;
