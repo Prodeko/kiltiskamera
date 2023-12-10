5ef427eb4b3c9fd5c64ed5c64c9ee06b180f9863
Leevi Jäkälä <jakala.leevi@gmail.com>
2023-12-10 16:37:29 +0200

    bigger emojis with space

--- app/frontend/src/Chat/Message.tsx
+++ app/frontend/src/Chat/Message.tsx
@@ -5,9 +5,9 @@ type ChatMessage = {
 
 const Message = ({ message }: { message: ChatMessage }) => {
   return (
-    <div className="text-gray-300">
-      <p className="text-xs">{message.timestamp}</p>
-      <p className="mb-3">{message.text}</p>
+    <div className="text-gray-300 pb-5">
+      <p className="text-3xl">{message.text}</p>
+      <p className="text-xs">{new Date(message.timestamp).toLocaleTimeString()}</p>
     </div>
   );
 };
