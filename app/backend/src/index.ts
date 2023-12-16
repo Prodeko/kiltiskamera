c4c22196438876298da453581263c5c3b8c19d18
aksun1 <aksunpostit@gmail.com>
2023-12-16 16:46:47 +0200

    Update cam url

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -90,7 +90,7 @@ app.get("/stream_url", ensureAuthenticated, (req: Request, res: Response) => {
   const token = addOnlineToken(user);
 
   console.log("got stream url name");
-  const m3u8StreamSource = `https://kiltiskamera.prodeko.org/live/hls/${token}/stream.m3u8`;
+  const m3u8StreamSource = `https://kiltis.prodeko.org/live/hls/${token}/stream.m3u8`;
 
   res.json({ url: m3u8StreamSource });
 });
