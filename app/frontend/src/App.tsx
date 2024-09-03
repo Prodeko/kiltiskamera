fbe9bcce457ef4173951ebf39c56a916cec0df51
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2024-09-03 16:04:24 +0300

    Revert two commits

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,65 +1,80 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import Chat from './Chat'
-import VideoLoading from './VideoLoading'
-import VideoJS from './VideoJs'
-import { Player } from 'video.js'
+import React, { useEffect, useRef, useState } from "react";
+import Chat from "./Chat";
+import VideoLoading from "./VideoLoading";
+
+import Hls from "hls.js";
 
 const App = () => {
-  const [videoStatus, setVideoStatus] = useState<boolean>(false)
-  const [streamUrl, setStreamUrl] = useState<string | null>(null)
+  const [videoStatus, setVideoStatus] = useState<boolean>(false);
 
-  const videoJsOptions = {
-    autoplay: true,
-    controls: true,
-    responsive: true,
-    fluid: true,
-    muted: true,
-    liveui: true,
-    sources: [
-      {
-        src: streamUrl,
-        type: 'application/x-mpegURL',
-      },
-    ],
-  }
+  const videoEl = useRef<HTMLVideoElement | null>(null);
 
-  const handlePlayerReady = (player: Player) => {
-    // You can handle player events here, for example:
-    player.on('ready', () => {
-      setVideoStatus(true)
-    })
+  useEffect(() => {
+    let newHls: Hls;
 
-    player.on('dispose', () => {
-      setVideoStatus(false)
-    })
-  }
+    const initializeHls = () => {
+      fetch("https://kiltiskamera.prodeko.org/stream_url") // Include http:// or https://
+        .then((response) => response.json())
+        .then((data) => {
+          const { url } = data;
 
-  const fetchStreamUrl = useCallback(() => {
-    fetch('https://kiltiskamera.prodeko.org/stream_url') // Include http:// or https://
-      .then((response) => response.json())
-      .then(async ({ url }) => {
-        setStreamUrl(url)
-      })
-      .catch((error) => {
-        console.error('Error fetching stream URL:', error)
-        console.log('Retrying...')
-        setTimeout(fetchStreamUrl, 1000)
-      })
-  }, [])
+          if (videoEl.current) {
+            const videoCurrent = videoEl.current;
+            if (
+              videoCurrent.canPlayType("application/vnd.apple.mpegurl")
+                === "probably" // This result type is ridiculous
+            ) {
+              // HLS is natively supported in Safari
+              videoCurrent.src = url;
+              videoCurrent.addEventListener("loadedmetadata", function () {
+                videoCurrent.play();
+              });
+            } else if (Hls.isSupported()) {
+              // For other browsers, use Hls.js
+              newHls = new Hls({ liveDurationInfinity: true });
+              newHls.attachMedia(videoCurrent);
+              newHls.on(Hls.Events.MEDIA_ATTACHED, () => {
+                newHls.loadSource(url);
+              });
+            } else {
+              console.error("HLS not supported on this browser");
+            }
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching stream URL:", error);
+        });
+    };
 
-  useEffect(fetchStreamUrl, [fetchStreamUrl])
+    initializeHls();
+
+    // Cleanup function
+    return () => {
+      if (newHls) {
+        newHls.destroy();
+      }
+    };
+  }, [videoEl]);
 
   return (
     <div className="flex flex-col w-screen h-screen bg-black relative">
       <div className="aspect-video overflow-hidden max-w-screen max-h-screen">
-        <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+        <video
+          className={`aspect-video bg-black ${videoStatus ? "" : "h-0 w-0"}`}
+          ref={videoEl}
+          muted
+          autoPlay
+          playsInline
+          onPlay={() => setVideoStatus(true)}
+          onPause={() => console.log("Teremos")}
+        ></video>
         {!videoStatus && <VideoLoading />}
       </div>
       <div className="absolute top-0 right-0 h-full w-[25%] min-w-[280px]">
         <Chat />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
